449ab1ea61c7be0150f5a8827e958a5d738f64f5
Darya.Liashchynskaya <Dashalesh040600>
2023-09-27 22:57:43 +0300

    Add typescript on the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Divine Desires
+      </header>
+    </div>
+  );
+}
+
+export default App;
