0f0d07860c052f887fd92e398a1ae05f1d428afc
Darya.Liashchynskaya <Dashalesh040600>
2023-09-28 16:05:59 +0300

    create Button lego ui

---
+++ src/components/lego/Button/Button.tsx
@@ -0,0 +1,17 @@
+import {ButtonThemes} from "../../../constants/button.constants";
+import './Button.scss';
+
+interface ButtonProps {
+    text:string;
+    theme:ButtonThemes;
+    width:number
+}
+
+export const Button= ({text,theme,width}:ButtonProps)=>{
+    return (
+        <button className={`button ${theme===ButtonThemes.light? 'whiteButtonTheme':'redButtonTheme'}`}
+                style={{width:`${width}%`}}>
+            {text}
+        </button>
+    )
+ }
