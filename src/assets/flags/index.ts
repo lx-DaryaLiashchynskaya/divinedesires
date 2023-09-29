d4b3569ad8c8d15ebe5ca091d8f6211ec20bbcb5
Darya.Liashchynskaya <Dashalesh040600>
2023-09-29 15:11:35 +0300

    flag dropdown with light theme

---
+++ src/assets/flags/index.ts
@@ -0,0 +1,7 @@
+export const frenchFlag = require('./french.png');
+export const germanFlag = require('./german.png');
+export const portugalFlag = require('./portugal.png');
+export const usaFlag = require('./usa.png');
+export const spainFlag = require('./spain.png');
+
+
