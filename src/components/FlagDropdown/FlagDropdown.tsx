5fa164d802035f61fc2c7abec174bb72f80792ed
Darya.Liashchynskaya <Dashalesh040600>
2023-09-30 00:20:03 +0300

    add styles for flag select on theme change

--- src/components/FlagDropdown/FlagDropdown.tsx
+++ src/components/FlagDropdown/FlagDropdown.tsx
@@ -1,39 +1,32 @@
 import './FlagDropdown.scss';
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import Select, {StylesConfig} from "react-select";
 import {ILanguage, languages} from "../../constants/language.constants";
 import {i18n} from "../../i18n";
+import {ThemeContext} from "../../context/themeContext";
+import {
+    AppThemes,
+    DARK_THEME,
+    LIGHT_THEME,
+    PRIMARY_RED_COLOR,
+    PRIMARY_RED_COLOR_HOVER
+} from "../../constants/theme.constants";
 
 const defaultLanguage:ILanguage = languages[0];
 
-const colourStyles: StylesConfig<ILanguage> = {
-    control: (styles) => ({ ...styles,  border:0, backgroundColor: 'white', boxShadow: 'none' }),
-    input:(styles) => ({ ...styles,caretColor:'transparent'}),
-    valueContainer: (styles) => ({ ...styles,padding:0, backgroundColor: 'white' }),
-    option: (styles, {  isSelected }) => {
-        return {
-            ...styles,
-            cursor:'pointer',
-            backgroundColor: isSelected
-                ? '#cf0a0a'
-                : undefined,
-
-            ':hover': {
-                ...styles[':hover'],
-                backgroundColor: isSelected
-                    ? undefined
-                    :'#fae7e7',
-            },
-        };
-    },
-    dropdownIndicator: (styles) => ({ ...styles, padding:'0 5px 0 0', color: '#cf0a0a'}),
-    indicatorsContainer: (styles) => ({ ...styles, padding: 0}),
-    singleValue: (styles) => ({ ...styles, margin:0 }),
-    menu: (styles) => ({ ...styles, margin:'0 0 0 -5px' ,}),
+const colourStyles: StylesConfig<ILanguage>={
+        control: (styles) => ({...styles, border: 0, boxShadow: 'none'}),
+        input: (styles) => ({...styles, caretColor: 'transparent'}),
+        valueContainer: (styles) => ({...styles, padding: 0}),
+        dropdownIndicator: (styles) => ({...styles, padding: '0 5px 0 0',}),
+        indicatorsContainer: (styles) => ({...styles, padding: 0}),
+        singleValue: (styles) => ({...styles, margin: 0}),
+        menu: (styles) => ({...styles, margin: '0 0 0 -5px',}),
 };
 
 export const FlagDropdown =()=>{
     const [selectedLanguage, setSelectedLanguage] = useState<ILanguage>(defaultLanguage);
+    const { theme } = useContext(ThemeContext);
 
     const onLanguageChange=(language:any)=>{
         i18n.changeLanguage(language.value)
@@ -48,6 +41,17 @@ export const FlagDropdown =()=>{
                 options={languages}
                 components={{IndicatorSeparator: ()=> null}}
                 formatOptionLabel={(language)=><img alt={''} src={language.flagImage} className='flagIcon'/>}
+                theme={(themeView) => ({
+                    ...themeView,
+                    colors: {
+                        ...themeView.colors,
+                        primary25: PRIMARY_RED_COLOR_HOVER,
+                        primary50: PRIMARY_RED_COLOR_HOVER,
+                        primary: PRIMARY_RED_COLOR,
+                        neutral0: theme===AppThemes.DARK? DARK_THEME: LIGHT_THEME,
+                        neutral20:theme===AppThemes.DARK? LIGHT_THEME: PRIMARY_RED_COLOR
+                    },
+                })}
             />
     );
 }
