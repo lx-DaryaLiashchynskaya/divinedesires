86d7dc6d5929f335fb54e8424cc37ba819c8347f
Darya.Liashchynskaya <Dashalesh040600>
2023-10-02 14:33:25 +0300

    add bold text for 'sex life' phrase (according to design)

--- src/screens/TestBeginningScreen/TestBeginningScreen.tsx
+++ src/screens/TestBeginningScreen/TestBeginningScreen.tsx
@@ -1,4 +1,4 @@
-import {useTranslation} from "react-i18next";
+import {Trans, useTranslation} from "react-i18next";
 import {hundredsOfThousands} from "../../assets/onBoardingPictures";
 import {Button} from "../../components/lego/Button/Button";
 import {ButtonThemes} from "../../constants/button.constants";
@@ -16,7 +16,9 @@ export const TestBeginningScreen =()=>{
             <div className={'testBeginningContainer'}>
                 <img src={hundredsOfThousands} width={'100%'} alt={''} className={'hundredsOfThousandsPicture'}/>
                 <h2 className={'testBeginningTitle'}>{t("testBeginningScreen.introductoryTitle").toString().toUpperCase()}</h2>
-                <p className={'testBeginningText'}>{t("testBeginningScreen.introductoryText")}</p>
+                <p className={'testBeginningText'}>
+                    <Trans t={t}>{t("testBeginningScreen.introductoryText")}</Trans>
+                </p>
             </div>
             <div className={'continueButton'}>
                 <Button text={getContinueButtonText()} theme={ButtonThemes.DARK} width={100} />
